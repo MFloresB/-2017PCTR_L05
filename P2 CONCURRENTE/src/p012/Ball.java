4272111dccaecbadb243d3be30133bad6719bcb8
Mario Flores <mfe0007@alu.ubu.es>
2017-03-06 09:50:24 +0100

    Updated Postconditions

--- P2 CONCURRENTE/src/p012/Ball.java
+++ P2 CONCURRENTE/src/p012/Ball.java
@@ -2,72 +2,78 @@ package p012;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
-//TODO Transform the code to be used safely in a concurrent context.  
-public class Ball extends Thread{
-       //TODO  Find an archive named Ball.png ?
-	private String Ball = "Ball.png"; 
 
-	private double x,y,dx,dy;
-	private double v,fi;
+//TODO Transform the code to be used safely in a concurrent context.  
+public class Ball extends Thread {
+	// TODO Find an archive named Ball.png ?
+	private String Ball = "Ball.png";
+
+	private double x, y, dx, dy;
+	private double v, fi;
 	private Image image;
 
 	public Ball() {
 		ImageIcon ii = new ImageIcon(this.getClass().getResource(Ball));
 		image = ii.getImage();
-		x = Billiards.Width/4-16;
-		y = Billiards.Height/2-16;
+		x = Billiards.Width / 4 - 16;
+		y = Billiards.Height / 2 - 16;
 		v = 5;
-		fi =  Math.random() * Math.PI * 2;
+		fi = Math.random() * Math.PI * 2;
 	}
 
 	public void move() {
-		v = v*Math.exp(-v/1000);
-		dx = v*Math.cos(fi);
-		dy = v*Math.sin(fi);
+		v = v * Math.exp(-v / 1000);
+		dx = v * Math.cos(fi);
+		dy = v * Math.sin(fi);
 		if (Math.abs(dx) < 1 && Math.abs(dy) < 1) {
 			dx = 0;
 			dy = 0;
 		}
-		x += dx;   
+		x += dx;
 		y += dy;
-		
-		assert (x <= Billiards.Width);	
-		assert (x >= 0);
-		assert (y <= Billiards.Height);	
-		assert (y >= 0);
-				
+
+		assert (x > Board.LEFTBOARD);
+		assert (x < Board.RIGHTBOARD);
+
+		assert (y > Board.BOTTOMBOARD);
+		assert (y < Board.TOPBOARD);
+
 	}
 
 	public void reflect() {
-		if (Math.abs(x + 32 - Board.RIGHTBOARD) <  Math.abs(dx)) {
+		if (Math.abs(x + 32 - Board.RIGHTBOARD) < Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
-		if (Math.abs(y + 32 - Board.BOTTOMBOARD) <  Math.abs(dy)) {
-			fi = - fi;
+		if (Math.abs(y + 32 - Board.BOTTOMBOARD) < Math.abs(dy)) {
+			fi = -fi;
 		}
-		if (Math.abs(x - Board.LEFTBOARD) <  Math.abs(dx)) {
+		if (Math.abs(x - Board.LEFTBOARD) < Math.abs(dx)) {
 			fi = Math.PI - fi;
 		}
-		if (Math.abs(y - Board.TOPBOARD) <  Math.abs(dy)) {
-			fi = - fi;
+		if (Math.abs(y - Board.TOPBOARD) < Math.abs(dy)) {
+			fi = -fi;
 		}
-		//TODO Check postcondition	
+		assert (x > Board.LEFTBOARD);
+		assert (x < Board.RIGHTBOARD);
+
+		assert (y > Board.BOTTOMBOARD);
+		assert (y < Board.TOPBOARD);
 	}
 
 	public int getX() {
-		return (int)x;
+		return (int) x;
 	}
-	
+
 	public int getY() {
-		return (int)y;
+		return (int) y;
 	}
-	
+
 	public double getFi() {
 		return fi;
 	}
 
 	public double getdr() {
-		return Math.sqrt(dx*dx+dy*dy);
+		return Math.sqrt(dx * dx + dy * dy);
 	}
 
 	public void setX(double x) {
@@ -83,4 +89,3 @@ public class Ball extends Thread{
 	}
 
 }
-
